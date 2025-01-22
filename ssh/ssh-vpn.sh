f143567dd43eb853986ca84b1a765bce5cd3695d
Altunnel <155318569+Altunnel@users.noreply.github.com>
2025-01-22 15:29:01 +0700

    update ssh/ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -384,13 +384,7 @@ cd
 cat> /etc/cron.d/auto_exp << END
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-0 0 * * * root /usr/local/sbin/xp
-END
-
-cat> /etc/cron.d/daily_backup << END
-SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-0 22 * * * root /usr/local/sbin/backup
+0 2 * * * root /usr/local/sbin/xp
 END
 
 cat >/etc/cron.d/logclean <<-END
